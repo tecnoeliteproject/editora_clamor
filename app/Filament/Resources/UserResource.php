3a5f05a49390bf7afef52e0b2e6d7b4accce0c01
Casiprata <casipratafelpaqui@gmail.com>
2025-01-22 19:45:45 +0100

    feat: mudança do icon de usuários

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
     protected static ?int $navigationSort = 2;
 
     public static function form(Form $form): Form
